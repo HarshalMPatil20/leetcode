49ab999d61121bc8687ad943b236be4700261c4c
Harshal Patil <108197082+HarshalMPatil20@users.noreply.github.com>
2024-03-24 15:20:52 +0530

    Time: 4 ms (85.61%) | Memory: 57 MB (87.09%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/find-the-duplicate-number.java
@@ -1,38 +1,39 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        // int slow = nums[0];
-        // int fast = nums[0];
+        int slow = nums[0];
+        int fast = nums[0];
  
-        // // 1st collision 
-        // // do while loop because slow and fast pointer are starting from same position
-        // // So if we use while loop , while checks condition first and then executes it
-        // // so it gets false on the first place without running loop
+        // 1st collision 
+        // do while loop because slow and fast pointer are starting from same position
+        // So if we use while loop , while checks condition first and then executes it
+        // so it gets false on the first place without running loop
 
-        // do {
-        //     slow = nums[slow];
-        //     fast = nums[nums[fast]]; // double speed of slow pointer
-        // } while(slow!=fast); 
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]]; // double speed of slow pointer
+        } while(slow!=fast); 
         
-        // // reset fast pointer to starting position
-        // fast = nums[0];
+        // reset fast pointer to starting position
+        fast = nums[0];
 
-        // // 2nd collision (fast pointer have same speed as slow pointer)
-        // while(slow != fast){
-        //     slow = nums[slow];
-        //     fast = nums[fast]; // S
-        // }
-        // return slow;
-        // // or you can return fast pointer as they are at same position
-        int [] freq  = new int[nums.length];
-
-        for(int i = 0; i < nums.length; i++){
-            freq[nums[i]]++;
-
-            if(freq[nums[i]] > 1){
-                return nums[i];
-            }
+        // 2nd collision (fast pointer have same speed as slow pointer)
+        while(slow != fast){
+            slow = nums[slow];
+            fast = nums[fast]; // S
         }
-        return -1;
+        return slow;
+        // or you can return fast pointer as they are at same position
+
+        // int [] freq  = new int[nums.length];
+
+        // for(int i = 0; i < nums.length; i++){
+        //     freq[nums[i]]++;
+
+        //     if(freq[nums[i]] > 1){
+        //         return nums[i];
+        //     }
+        // }
+        // return -1;
 
 
 
