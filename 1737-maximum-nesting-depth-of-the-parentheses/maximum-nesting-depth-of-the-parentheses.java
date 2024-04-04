16ed1cfa85bdabef70f0f06ff19ed94ae66fcd7e
Harshal Patil <108197082+HarshalMPatil20@users.noreply.github.com>
2024-04-04 09:59:35 +0530

    Time: 1 ms (33.14%) | Memory: 40.9 MB (96.39%) - LeetSync

--- 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.java
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.java
@@ -2,22 +2,16 @@ class Solution {
     public int maxDepth(String s) {
         
         int balance=0;
+        int maximum=0;
         for(int i=0; i<s.length();i++){
             if(s.charAt(i)=='(')balance++;
             if(s.charAt(i)==')') balance--;
+             maximum=Math.max(maximum,balance);
         }
 
         if(balance !=0) return -1;
-        else{
-            int counter=0;
-            int maximum=0;
-            for(int i=0;i<s.length();i++){
-                if(s.charAt(i)=='(')counter++;
-                if(s.charAt(i)==')') counter--;
-                maximum=Math.max(maximum,counter);
-            }
-            return maximum;
-        }
+        else return maximum;
+        
         
     }
 }
