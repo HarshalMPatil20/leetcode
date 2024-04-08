02cf2c74176de1d7edb6ba3b21d602f790d4002a
Harshal Patil <108197082+HarshalMPatil20@users.noreply.github.com>
2024-04-08 16:46:43 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (61.63%) - LeetSync

--- 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.java
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.java
@@ -1,11 +1,17 @@
 class Solution {
+
     public int countStudents(int[] students, int[] sandwiches) {
-        int count_0=0;
-        int count_1=0;
+        int count_0=0; // count of 0 in students
+        int count_1=0; // count of 1 in students
+
+// Generating count numbers from students of 0 & 1
         for(int i=0;i<students.length;i++){
             if(students[i]==0) count_0++;
             else count_1++;
         }
+
+// Iterating through sandwiches at any point when any of numbers from students array is become zero 
+//so other sandwitches are of reamaining students as students and sandwiches are of same number
         int i=0;
         while(count_0>=0 && count_1>=0 && i<sandwiches.length){
             if(sandwiches[i]==0) count_0--;
@@ -13,9 +19,8 @@ class Solution {
             i++;
         }
 
-        // if(i==sandwiches.length) return 0;
-        // return sandwiches.length-i+1;
-        if( count_0<0) return count_1;
+// if count of 0 becmes negative means it sold already so count of 1 are remaining students and vice versa
+        if( count_0 < 0) return count_1;
         return count_0; 
         
     }
