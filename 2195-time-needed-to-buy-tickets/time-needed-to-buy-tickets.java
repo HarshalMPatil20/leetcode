08fb94e6781b7af3d4072d35f6e20680264b01f1
Harshal Patil <108197082+HarshalMPatil20@users.noreply.github.com>
2024-04-09 19:51:54 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (47.55%) - LeetSync

--- 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.java
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.java
@@ -1,24 +1,32 @@
 class Solution {
     public int timeRequiredToBuy(int[] tickets, int k) {
 
+// Targeted Element already stored because we are going to change values in array
         int element=tickets[k];
 
+// Subtracted the targeted element from every element of array
         for(int i=0;i<tickets.length;i++){
             tickets[i]-=element;
         }
+// positive variable stores number of positive and 0 element after the targeted element becomes zero
+// Because according to question we have to stop time counter after targeted element becomes 0 hence we have to subtract counter of extra element whichb we have considered in multiplication
+        int positive_elements=0;
 
-        int positive=0;
-        int negative=0;
+// negative counter stores the addition of all extra iterated element even after they becomes 0
+        int negative_counters=0;
 
+// LOOP - for determining the number of positive and 0 elements after targted element excluding targted element index
         for(int i=k+1;i<tickets.length;i++){
-            if(tickets[i]>=0)  positive++;
+            if(tickets[i]>=0)  positive_elements++;
         }
 
+// LOOP - For aggregating all negative counters which are extra counted due to direct subtraction of targrted element from that element
         for(int i=0;i<tickets.length;i++){
-            if(tickets[i]<0) negative += tickets[i];
+            if(tickets[i]<0) negative_counters += tickets[i];
         }
 
-        return (element*tickets.length)+negative-positive;
+// CALCULATION - Total number of iterations of targeted element in worst case + all negative elements counters (plus because the negative_counters is alredy negative in sign) - extra iteated element even after nullification of targeted element
+        return (element*tickets.length) + negative_counters - positive_elements;
         
     }
 }
