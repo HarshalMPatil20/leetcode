abe3da60b1ef552110a4a6789b27f189aceab323
Harshal Patil <108197082+HarshalMPatil20@users.noreply.github.com>
2024-04-10 11:35:42 +0530

    Time: 3 ms (97.57%) | Memory: 43.2 MB (86.41%) - LeetSync

--- 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.java
+++ 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.java
@@ -1,9 +1,14 @@
 class Solution {
-    public int Find_next_empty_space(int[] arr,int k){
-        int i=k;
+
+
+// Function to find next empty space ( leaving an empty space in between )
+
+    public int Find_next_empty_space(int[] arr,int Current_position){
+        int i = Current_position; 
         int Mod_2=0;
+
         while(true){
-            if(arr[i % arr.length]==0) Mod_2 ++;
+            if(arr[i % arr.length]==0) Mod_2++;
             if(Mod_2 % 2==0 && Mod_2!=0) return i % arr.length;
             i++;
         }
@@ -14,13 +19,16 @@ class Solution {
         Arrays.sort(deck);
 
         int [] ans = new int [deck.length];
-        int s=1;
+        int Current_position=1;
 
+// first element will be same for both and according to our first ekement from deck will be get placed in 2nd position in ans which will be wrong hence....
     ans[0]=deck[0];
 
+// placing every element one by one in ans array from deck
+
     for(int i=1;i<deck.length;i++){
-        s=Find_next_empty_space(ans,s);
-        ans[s]=deck[i];
+        Current_position=Find_next_empty_space(ans,Current_position);
+        ans[Current_position]=deck[i];
     }
 
       return ans;  
