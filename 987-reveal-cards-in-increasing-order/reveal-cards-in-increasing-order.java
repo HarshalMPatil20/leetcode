58dadf71bb8d92410c05006e2ef3a0b14d73fffe
Harshal Patil <108197082+HarshalMPatil20@users.noreply.github.com>
2024-04-10 11:17:32 +0530

    Time: 3 ms (97.57%) | Memory: 43.1 MB (90.78%) - LeetSync

--- 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.java
+++ 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.java
@@ -1,10 +1,10 @@
 class Solution {
     public int Find_next_empty_space(int[] arr,int k){
         int i=k;
-        int b=0;
+        int Mod_2=0;
         while(true){
-            if(arr[i % arr.length]==0) b++;
-            if(b%2==0 && b!=0) return i % arr.length;
+            if(arr[i % arr.length]==0) Mod_2 ++;
+            if(Mod_2 % 2==0 && Mod_2!=0) return i % arr.length;
             i++;
         }
 
@@ -14,13 +14,15 @@ class Solution {
         Arrays.sort(deck);
 
         int [] ans = new int [deck.length];
-        int s=0;
+        int s=1;
 
     ans[0]=deck[0];
+
     for(int i=1;i<deck.length;i++){
         s=Find_next_empty_space(ans,s);
         ans[s]=deck[i];
     }
+
       return ans;  
     }
 }
