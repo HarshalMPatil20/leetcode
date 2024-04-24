040d2b728dc728ceafc64e2177ac0c502d0dfabc
Harshal Patil <108197082+HarshalMPatil20@users.noreply.github.com>
2024-04-24 12:02:46 +0530

    Time: 0 ms (100.00%) | Memory: 40 MB (83.94%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int fib(int n) {
+        if(n==0) return 0;
+        if(n==1) return 1;
+        
+        int t0 = 0;
+        int t1 = 1;
+        int t2 = 0;
+
+        for(int i=0;i<n-1;i++){
+            t2 = t0 + t1;
+            t0=t1;
+            t1=t2;
+        }
+        return t2;
+    }
+}
