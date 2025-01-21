3c3bb4129ed6fcd692746b38480739608d22403f
Harshal Patil <harshalmpatil210@gmail.com>
2025-01-21 23:08:12 +0530

    Time: 10 ms (24.13%) | Memory: 57.2 MB (86.61%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -8,52 +8,29 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-
 class Solution {
-    // Function to Reverse Linked List given Head Pointer
-    public ListNode reverseList(ListNode head) {
-        ListNode prev = null;
-        ListNode curr = head;
-
-        while(curr != null){
-            ListNode next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
-        }
-        return prev;
-    }
-
     public boolean isPalindrome(ListNode head) {
-        // Step 1 - Find the middle of linked list
-        ListNode slow = head;
-        ListNode fast = head;
 
-        while( fast.next != null && fast.next.next != null){
-            slow = slow.next;
-            fast = fast.next.next;
+        Stack<Integer> st = new Stack<>();
+
+        ListNode p1=head;
+        ListNode p2=head;
+
+        while(p2!=null && p2.next!=null){
+            st.push(p1.val);
+            p1=p1.next;
+            p2=p2.next.next;
         }
-        
-        // now middle of list = slow.next
-        // Step 2 - Reverse the linked list from Middle
-        ListNode new_head = reverseList(slow.next);
 
-        // Step 3 - Take to temperory pointers for first head and second head and Compare the list
-        ListNode first = head;
-        ListNode second = new_head;
-
-        while(second != null){
-
-            if(first.val != second.val){
-                reverseList(new_head);
-                return false;
-            }
-            first= first.next;
-            second = second.next;
+        if(p2!=null){   //for odd number of nodes in linked list
+            p1=p1.next;
         }
-       // Step 4 - Reverse the second half of list again to make it as it was!
-        reverseList(new_head);
-         return true;
 
+        while(p1!=null){
+            if(st.pop()!=p1.val) return false;
+            p1=p1.next;
+        }
+
+        return true;
     }
 }
