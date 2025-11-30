8b3c1489d4bc450539c1043762f4817d4e183c15
Harshal Patil <harshalmpatil210@gmail.com>
2025-11-30 19:14:02 +0530

    Time: 2 ms (7.46%) | Memory: 43.4 MB (27.60%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -1,27 +1,20 @@
 class Solution {
-
-    public List<Integer> generateRows (int row){
-        int element = 1;
-        // In java while Initialize the List
-        // Object is created 
-        List<Integer> ansRow = new ArrayList<Integer>();
-         
-        ansRow.add(element);
-
-        for(int i=1; i<row; i++){
-            element = element * (row-i);
-            element = element / i;
-            ansRow.add(element);
-        } 
-        return ansRow;
-    }
     public List<List<Integer>> generate(int numRows) {
-        // This is how we Initialize list of lists
-        List<List<Integer>> Ans = new ArrayList<List<Integer>>(); 
 
-        for(int i=1; i<= numRows; i++){
-            Ans.add(generateRows(i));
+        List<List<Integer>> triangle = new ArrayList<>();
+
+        for(int i=0;i<numRows;i++){
+             
+             List<Integer> row = new ArrayList<>(Collections.nCopies(i+1,1));
+
+             for(int j=1;j<i;j++){
+                row.set(j, triangle.get(i-1).get(j-1)+ triangle.get(i-1).get(j));
+             }
+
+             triangle.add(row);
         }
-        return Ans;
+
+        return triangle;
+        
     }
 }
