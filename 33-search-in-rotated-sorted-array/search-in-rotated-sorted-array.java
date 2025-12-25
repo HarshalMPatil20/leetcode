aa53b5ca7e9b52ce33bcd719360a626f03f8ad20
Harshal Patil <harshalmpatil210@gmail.com>
2025-12-26 00:24:09 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (54.17%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,47 +1,33 @@
 class Solution {
-    public int findPivot(int[] nums) {
-        int low = 0, high = nums.length - 1;
+    public int search(int[] nums, int target) {
 
-        while (low < high) {
-            int mid = (low + high) / 2;
+        int low=0;
+        int high=nums.length-1;
 
-            if (nums[mid] < nums[high]) {
-                // Pivot is in right half
-                high = mid;
-                
-            } else {
-                // Pivot is in left half (including mid)
-                low = mid+1;
+        while(low<=high){
+
+            int mid = (low+high)/2;
+
+            if(nums[mid]==target) return mid;
+
+            if(nums[low]<=nums[mid]){
+                if(nums[low]<=target && target<=nums[mid]){
+                    high=mid-1;
+                }
+                else{
+                    low=mid+1;
+                }
+            }
+            else{
+                if(nums[mid]<=target && target<=nums[high]){
+                    low=mid+1;
+                }
+                else{
+                    high=mid-1;
+                }
             }
         }
-        return high; // index of smallest element
-    }
-
-    public int binarySearch(int[] nums, int low, int high, int target) {
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            if (nums[mid] == target)
-                return mid;
-            else if (nums[mid] < target)
-                low = mid + 1;
-            else
-                high = mid - 1;
-        }
         return -1;
-    }
-
-    public int search(int[] nums, int target) {
-        int n = nums.length;
-
-        int pivot = findPivot(nums);
-
-        // If target is in right sorted part
-        if (target >= nums[pivot] && target <= nums[n - 1]) {
-            return binarySearch(nums, pivot, n - 1, target);
-        }
-        // Else target is in left sorted part
-        else {
-            return binarySearch(nums, 0, pivot - 1, target);
-        }
+        
     }
 }
