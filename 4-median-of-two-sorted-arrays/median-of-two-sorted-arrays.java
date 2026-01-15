1360e78b33adaa4ac7800050c6be128c956bbe62
Harshal Patil <harshalmpatil210@gmail.com>
2026-01-15 18:39:58 +0530

    Time: 1 ms (99.56%) | Memory: 48.9 MB (60.23%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,66 +1,40 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int[] a = nums1;
+        int[] b = nums2;
+        if(a.length>b.length) return findMedianSortedArrays(b,a);
 
-        int finalLength = nums1.length + nums2.length;
+        int low = 0;
+        int high = a.length;
 
-        int index2 = finalLength / 2;
-        int idx2 = 0;
-        int idx1 = 0;
+        while(low<=high){
 
-        int index1 = index2 - 1;
+            int part1 = low+(high-low)/2;
+            int part2 = (a.length+b.length+1)/2-part1;
 
-        int ptr1 = 0;
+            int l1 = (part1==0)?Integer.MIN_VALUE:a[part1-1];
+            int l2 = (part2==0)?Integer.MIN_VALUE:b[part2-1];
+            int r1 = (part1==a.length)?Integer.MAX_VALUE:a[part1];
+            int r2 = (part2==b.length)?Integer.MAX_VALUE:b[part2];
 
-        int ptr2 = 0;
-
-        int count = 0;
-
-        while (ptr1 < nums1.length && ptr2 < nums2.length) {
-            if (nums1[ptr1] >= nums2[ptr2]) {
-                if (count == index1)
-                    idx1 = nums2[ptr2];
-                if (count == index2)
-                    idx2 = nums2[ptr2];
-                ptr2++;
-                count++;
-
-            } else {
-                if (count == index1)
-                    idx1 = nums1[ptr1];
-                if (count == index2)
-                    idx2 = nums1[ptr1];
-                ptr1++;
-                count++;
-
+            if(l1<=r2 && l2<=r1){
+                if((a.length+b.length)%2==1){
+                    return (double)Math.max(l1,l2);
+                }
+                else{
+                    return ((double)(Math.max(l1,l2)+Math.min(r1,r2))/2);
+                }
             }
+            else if(l1>r2){
+                high = part1-1;
+            }
+            else{
+                low = part1+1;
+            } 
+
         }
 
-        while (ptr1 < nums1.length) {
-            
-            if (count == index1)
-                idx1 = nums1[ptr1];
-            if (count == index2)
-                idx2 = nums1[ptr1];
-
-            ptr1++;
-            count++;
-        }
-
-        while (ptr2 < nums2.length) {
-            
-            if (count == index1)
-                idx1 = nums2[ptr2];
-            if (count == index2)
-                idx2 = nums2[ptr2];
-
-            ptr2++;
-            count++;
-        }
-
-        if(finalLength%2==1) return (double) idx2;
-        else{
-            return (double)(idx1+idx2)/2;
-        }
-
+        return 0.0;
+        
     }
 }
