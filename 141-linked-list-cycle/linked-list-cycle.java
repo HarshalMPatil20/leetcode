31e62693d42fb2c4670b93b889234d8c592a7e2f
Harshal Patil <harshalmpatil210@gmail.com>
2026-01-26 22:48:06 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (63.58%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -11,32 +11,17 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-          // Initialize a pointer 'temp'
-        // at the head of the linked list
-        ListNode temp = head;
 
-        // Create a map to keep track of
-        // encountered nodes
-        HashSet<ListNode> nodeMap = new HashSet<>();
+       ListNode slow = head;
+       ListNode fast = head;
 
-        // Step 2: Traverse the linked list
-        while (temp != null) {
-            // If the node is already in the
-            // map, there is a loop
-            if (nodeMap.contains(temp)) {
-                return true;
-            }
-            // Store the current node
-            // in the map
-            nodeMap.add(temp);
+       while(fast != null && fast.next != null){
+        fast = fast.next.next;
+        slow = slow.next;
+        if(slow == fast) return true;
+       }
 
-            // Move to the next node
-            temp = temp.next;
-        }
-
-        // Step 3: If the list is successfully traversed 
-        // without a loop, return false
-        return false;
+       return false; 
         
     }
 }
