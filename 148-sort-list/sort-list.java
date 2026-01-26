bac67b8f9c9f44ba7be1c09644d2096c61baeb20
Harshal Patil <harshalmpatil210@gmail.com>
2026-01-26 18:51:15 +0530

    Time: 48 ms (5.21%) | Memory: 63.7 MB (12.09%) - LeetSync

---
+++ 148-sort-list/sort-list.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        ListNode curr = head;
+
+        while(curr != null){
+            pq.add(curr.val);
+            curr = curr.next;
+        }
+
+        curr = head;
+        while(curr != null){
+            curr.val = pq.poll();
+            curr = curr.next;
+        }
+
+        return head;
+        
+    }
+}
