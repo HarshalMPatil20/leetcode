4ce953939fa98e2e0d0956d89b845d1ac211de5e
Harshal Patil <harshalmpatil210@gmail.com>
2026-01-29 00:47:39 +0530

    Time: 41 ms (6.02%) | Memory: 108 MB (5.06%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -11,26 +11,33 @@
 class Solution {
     public boolean isPalindrome(ListNode head) {
 
-        Stack<Integer> st = new Stack<>();
+        ListNode slow = head;
+        ListNode fast = head;
 
-        ListNode p1=head;
-        ListNode p2=head;
-
-        while(p2!=null && p2.next!=null){
-            st.push(p1.val);
-            p1=p1.next;
-            p2=p2.next.next;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
         }
 
-        if(p2!=null){   //for odd number of nodes in linked list
-            p1=p1.next;
+        System.out.println(slow.val);
+
+        fast = head;
+
+        Stack <Integer> s = new Stack<>();
+
+        while(slow != null){
+            s.add(slow.val);
+            slow = slow.next;
         }
 
-        while(p1!=null){
-            if(st.pop()!=p1.val) return false;
-            p1=p1.next;
+        System.out.println(s);
+
+        while(!s.isEmpty()){
+            if(fast.val != s.pop()) return false;
+            fast = fast.next;
         }
 
         return true;
+        
     }
 }
