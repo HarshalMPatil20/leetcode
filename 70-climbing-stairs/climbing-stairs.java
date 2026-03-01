e706d4361407ef5ac16bea5e7a4769ba3b93be80
Harshal Patil <harshalmpatil210@gmail.com>
2026-03-02 01:29:13 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (87.66%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,22 +1,16 @@
 class Solution {
-    public int fun(int i, int n, int[] dp) {
-        if (i > n)
-            return 0;
-        if (i == n)
-            return 1;
-        if (dp[i] != -1)
-            return dp[i];
+    public int function(int i, int[] dp){
+        if(dp[i] != 0) return dp[i];
 
-        return dp[i] = fun(i + 1, n, dp) + fun(i + 2, n, dp);
+        if(i == 0 || i == 1) return dp[i] = 1;
+
+        return dp[i] = function(i-1,dp)+function(i-2,dp);
     }
-
     public int climbStairs(int n) {
 
-        int[] dp = new int[n + 1];
+        int[] dp  = new int[n+1];
 
-        Arrays.fill(dp, -1);
-
-        return fun(0, n, dp);
-
+        return function(n , dp);
+        
     }
 }
