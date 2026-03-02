e1f841683fb46178b013a2525cadd9ec3c3ec876
Harshal Patil <harshalmpatil210@gmail.com>
2026-03-02 20:57:20 +0530

    Time: 5 ms (10.83%) | Memory: 49.5 MB (13.88%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,23 +1,43 @@
 class Solution {
-    public void DFS(int node, int[] visited,int[][] isConnected){
-        visited[node] = 1;
+    public void DFS(int node, boolean[] visited,int[][] isConnected){
+        visited[node] = true;
 
         for(int i=0;i<isConnected.length;i++){
-            if((isConnected[node][i] == 1) && (visited[i] == 0)){
+            if((isConnected[node][i] == 1) && (!visited[i])){
                 DFS(i,visited,isConnected);
             }
         }
+    }
+
+    public void BFS(int node, boolean[] visited,int[][] isConnected){
+        Queue<Integer> queue = new LinkedList<>();
+
+        queue.add(node);
+        
+        while(!queue.isEmpty()){
+
+            int parent = queue.poll();
+            visited[parent] = true;
+
+            for(int i = 0; i<isConnected.length; i++){
+                if((isConnected[parent][i] == 1) && (!visited[i])){
+                    queue.add(i);
+                }
+            }
+
+        }
+
     }
     public int findCircleNum(int[][] isConnected) { 
 
         int provinces = 0;
 
-        int[] visited = new int[isConnected.length];
+        boolean[] visited = new boolean[isConnected.length];
 
         for(int i = 0; i<visited.length; i++){
-            if(visited[i] == 0){
+            if(!visited[i]){
                 provinces++;
-                DFS(i,visited,isConnected);
+                BFS(i,visited,isConnected);
             }
         }
 
