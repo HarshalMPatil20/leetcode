8da1edabd4f11c5e1cfbdba13909eea9920c4e45
Harshal Patil <harshalmpatil210@gmail.com>
2026-03-07 03:25:15 +0530

    Time: 1 ms (97.17%) | Memory: 43.6 MB (84.67%) - LeetSync

--- 1032-satisfiability-of-equality-equations/satisfiability-of-equality-equations.java
+++ 1032-satisfiability-of-equality-equations/satisfiability-of-equality-equations.java
@@ -1,39 +1,49 @@
 class Solution {
 
-    public void assign(char A, char B, int[] charValue) {
-
-        int changeTo = charValue[B - 'a'];
-            for (int i = 0; i < charValue.length; i++) {
-                if (charValue[i] == changeTo) {
-                    charValue[i] = charValue[A - 'a'];
-                }
-            }
-
+    public int find(int node,int[] parent){
+        if(parent[node] == node) return node;
+        return parent[node] = find(parent[node],parent);
     }
 
+    public void union(int x, int y,int[] parent,int[] rank){
+        int x_parent = find(x,parent);
+        int y_parent = find(y,parent);
+
+        if(rank[x_parent] == rank[y_parent]){
+            parent[y_parent] = x_parent;
+            rank[x_parent]++;
+        }
+        else if(rank[x_parent]>rank[y_parent]){
+            parent[y_parent] = x_parent;
+        }
+        else{
+            parent[x_parent] = y_parent;
+        }
+    }
+    
     public boolean equationsPossible(String[] equations) {
 
-        int[] charValue = new int[26];
+        int[] parent = new int[26];
+        int[] rank = new int[26];
 
-        for (int i = 0; i < charValue.length; i++) {
-            charValue[i] = i;
+        for(int i = 0; i<parent.length; i++){
+            parent[i] = i;
         }
 
-        for (String s : equations) {
-            if (s.charAt(1) == '=') {
-                assign(s.charAt(0), s.charAt(3), charValue);
+        for(String s : equations){
+            if(s.charAt(1)=='='){
+                union(s.charAt(0)-'a',s.charAt(3)-'a',parent,rank);
             }
         }
-
-        for (String s : equations) {
-            if (s.charAt(1) == '!') {
-                if (charValue[s.charAt(0) - 'a'] == charValue[s.charAt(3) - 'a']) {
+        for(String s : equations){
+            if(s.charAt(1)=='!'){
+                if(find(s.charAt(0)-'a',parent) == find(s.charAt(3)-'a',parent)){
                     return false;
-                }
+                };
             }
         }
 
         return true;
-
+        
     }
 }
